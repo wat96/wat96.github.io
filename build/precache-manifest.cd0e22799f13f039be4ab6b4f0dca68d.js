587873c9b5aba88690a80c9b7eb0307fa98b86a9
Wat96 <wat9691@gmail.com>
2022-06-18 21:14:16 -0700

    dalle

---
+++ build/precache-manifest.cd0e22799f13f039be4ab6b4f0dca68d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83e4fccb87052abcd124a7d4d9850fb5",
+    "url": "/dalle-playground/index.html"
+  },
+  {
+    "revision": "61e9e94c2d2b18dae5bf",
+    "url": "/dalle-playground/static/css/main.92e41723.chunk.css"
+  },
+  {
+    "revision": "87a862414a4a95f2d889",
+    "url": "/dalle-playground/static/js/2.5dfdb73e.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/dalle-playground/static/js/2.5dfdb73e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61e9e94c2d2b18dae5bf",
+    "url": "/dalle-playground/static/js/main.9c0bec05.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/dalle-playground/static/js/main.9c0bec05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2287b2335ed2078787ec",
+    "url": "/dalle-playground/static/js/runtime-main.f1925457.js"
+  }
+]);
